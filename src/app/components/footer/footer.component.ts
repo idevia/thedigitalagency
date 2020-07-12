69344447e5a676e8a1a026c1d3ad7dedd5e2e109
Mortuz <mortuzalam@gmail.com>
2020-07-12 20:29:51 +0530

    add footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  socialLinks = [
+    {
+      image: 'http://thedigitalagency.london/media/1001/facebook.png',
+      link: '#!',
+    },
+    {
+      image: 'http://thedigitalagency.london/media/1002/twitter.png',
+      link: '#!',
+    },
+    {
+      image: 'http://thedigitalagency.london/media/1003/youtube.png',
+      link: '#!',
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
